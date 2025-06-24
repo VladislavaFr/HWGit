5a7e84d0c4a83bfc6a42153ffe66753a914a3067
VladislavaFn <vladislava.fridmann1@aol.com>
2025-06-24 20:50:31 +0300

    add mask_account_card

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from src.masks import get_mask_card_number
 from src.masks import get_mask_account
+from src.widget import get_date
+from src.widget import mask_account_card
 
 card_number = "7000792289606361"
 masked_card_number = get_mask_card_number(card_number)
@@ -8,3 +10,13 @@ print(masked_card_number)
 account_number = "73654108430135874305"
 masked_account_number = get_mask_account(account_number)
 print(masked_account_number)
+
+card1 = "Maestro 1596837868705199"
+mask_card1 = mask_account_card(card1)
+card2 = "Visa Classic 6831982476737658"
+mask_card2 = mask_account_card(card2)
+
+acc1 = "Maestro 1596837868705199"
+mask_acc1 = mask_account_card(acc1)
+acc2 = "Visa Classic 6831982476737658"
+mask_acc2 = mask_account_card(acc2)
--- src/widget.py
+++ src/widget.py
@@ -2,12 +2,10 @@ def mask_account_card(name_and_number_of_card: str) -> str | None:
     """Функция маскировки имени и номера/счета банковской карты"""
     string_split = name_and_number_of_card.split(" ")
     numbers = string_split[-1]
-    if (len(numbers) != 16 or (not numbers.isdigit())) or (len(numbers) != 20 or (not numbers.isdigit())):
-        print("Попробуй снова")
-    elif len(numbers) == 16 or (not numbers.isdigit()):
-        return f"{string_split[:-1]} {numbers[:4]} {numbers[4:6]}** **** {numbers[-4:]}"
+    if len(numbers) == 16 or (not numbers.isdigit()):
+        print(f"{' '.join(string_split[:-1])} {numbers[:4]} {numbers[4:6]}** **** {numbers[-4:]}")
     elif len(numbers) == 20 or (not numbers.isdigit()):
-        return f"{string_split[:-1]} **{numbers[-4:]}"
+        print(f"{' '.join(string_split[:-1])} **{numbers[-4:]}")
 
 
 
