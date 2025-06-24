b72684a01dd63aa80e9591345ebcec3381ea9189
VladislavaFn <vladislava.fridmann1@aol.com>
2025-06-24 21:02:31 +0300

    add get_date

--- src/widget.py
+++ src/widget.py
@@ -10,5 +10,7 @@ def mask_account_card(name_and_number_of_card: str) -> str | None:
 
 
 def get_date(user_date):
+    """Функция которая принимает на вход строку (например:"2024-03-11T02:26:18.671407")
+    и выводить:"ДД.ММ.ГГГГ"""
     only_date = user_date[0:10]
     print(f"{only_date[8:10]}.{only_date[5:7]}.{only_date[0:4]}")
