9ebb8471fe67ea925f2dff318da81425d8780d43
VladislavaFn <vladislava.fridmann1@aol.com>
2025-06-24 20:28:39 +0300

    add mask_account_card

---
+++ src/widget.py
@@ -0,0 +1,15 @@
+def mask_account_card(name_and_number_of_card: str) -> str | None:
+    """Функция маскировки имени и номера/счета банковской карты"""
+    string_split = name_and_number_of_card.split(" ")
+    numbers = string_split[-1]
+    if (len(numbers) != 16 or (not numbers.isdigit())) or (len(numbers) != 20 or (not numbers.isdigit())):
+        print("Попробуй снова")
+    elif len(numbers) == 16 or (not numbers.isdigit()):
+        return f"{string_split[:-1]} {numbers[:4]} {numbers[4:6]}** **** {numbers[-4:]}"
+    elif len(numbers) == 20 or (not numbers.isdigit()):
+        return f"{string_split[:-1]} **{numbers[-4:]}"
+
+
+
+def get_date():
+    pass
