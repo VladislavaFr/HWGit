a11edcef03688bf0867797b3541a01a9b383bd16
VladislavaFn <vladislava.fridmann1@aol.com>
2025-06-26 20:14:11 +0300

    add

--- src/widget.py
+++ src/widget.py
@@ -1,16 +1,17 @@
-def mask_account_card(name_and_number_of_card: str) -> str | None:
+from src.masks import get_mask_account, get_mask_card_number
+
+def mask_account_card(name_and_number_of_card: str) -> str:
     """Функция маскировки имени и номера/счета банковской карты"""
     string_split = name_and_number_of_card.split(" ")
     numbers = string_split[-1]
     if len(numbers) == 16 or (not numbers.isdigit()):
-        print(f"{' '.join(string_split[:-1])} {numbers[:4]} {numbers[4:6]}** **** {numbers[-4:]}")
+        return f"{' '.join(string_split[:-1])} {get_mask_card_number(numbers)}"
     elif len(numbers) == 20 or (not numbers.isdigit()):
-        print(f"{' '.join(string_split[:-1])} **{numbers[-4:]}")
+        return f"{' '.join(string_split[:-1])} {get_mask_account(numbers)}"
 
 
-
-def get_date(user_date):
+def get_date(user_date: str) -> str:
     """Функция которая принимает на вход строку (например:"2024-03-11T02:26:18.671407")
     и выводить:"ДД.ММ.ГГГГ"""
     only_date = user_date[0:10]
-    print(f"{only_date[8:10]}.{only_date[5:7]}.{only_date[0:4]}")
+    return f"{only_date[8:10]}.{only_date[5:7]}.{only_date[0:4]}"
