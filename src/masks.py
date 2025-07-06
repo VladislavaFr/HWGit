717b2ea4f36f4860ae3ec0db4a191b4712f6798a
VladislavaFn <vladislava.fridmann1@aol.com>
2025-07-06 22:54:31 +0300

    add processing

--- src/masks.py
+++ src/masks.py
@@ -1,14 +1,12 @@
 def get_mask_card_number(card_number: str) -> str | None:
-    """Функция маскировки номера банковской карты"""
-    if len(card_number) != 16 or (not card_number.isdigit()):
-        print("Попробуйте снова")
-    else:
-        return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+    """Маскирует номер банковской карты. Формат: ХХХХ ХХ** **** ХХХХ"""
+    if len(card_number) != 16 or not card_number.isdigit():
+        raise ValueError("Номер карты должен быть строкой из 16 цифр")
+    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
 
 
-def get_mask_account(account_number):
-    """Функция маскировки номера банковского счета"""
-    if len(account_number) != 20 or (not account_number.isdigit()):
-        print("Попробуйте снова")
-    else:
-        return f"**{account_number[-4:]}"
+def get_mask_account(account_number: str) -> str:
+    """Маскирует номер банковского счета. Формат: **ХХХХ"""
+    if len(account_number) != 20 or not account_number.isdigit():
+        raise ValueError("Номер счета должен быть строкой из 20 цифр")
+    return f"**{account_number[-4:]}"
